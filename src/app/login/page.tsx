5de6c552241153c2ab8a189f566b5418c6832e75
elmaikart <elmaikart@gmail.com>
2025-09-25 23:03:38 -0300

    Fix imports for Button and Input

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,8 @@
 // src/app/login/page.tsx
 
 import React from "react";
-import  Input from "@/components/input";
-import  Button from "@/components/button";
-
+import Input from "@/components/input";
+import Button from "@/components/button";
 export default function LoginPage() {
   return (
     <div className="min-h-screen flex flex-col bg-[#FFF5D7]">
